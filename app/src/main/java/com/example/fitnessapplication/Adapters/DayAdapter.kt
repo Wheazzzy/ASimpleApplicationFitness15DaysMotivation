c1c8f81abb2b5e381638f0e5c05e03ced9362929
XXAUROR <200103001@stu.sdu.edu.kz>
2022-11-21 21:13:48 +0600

    Created function setting Data DataHolder

--- app/src/main/java/com/example/fitnessapplication/Adapters/DayAdapter.kt
+++ app/src/main/java/com/example/fitnessapplication/Adapters/DayAdapter.kt
@@ -1,6 +1,18 @@
 package com.example.fitnessapplication.Adapters
 
+import android.view.View
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fitnessapplication.databinding.DaysListItemBinding
 
-class DayAdapter: ListAdapter {
+class DayAdapter: ListAdapter<DayModel, > {
+
+    class DayHolder(view: View): RecyclerView.ViewHolder(view){
+        private val binding = DaysListItemBinding.bind(view)
+        fun settingData(day: DayModel) = with(binding){
+            names.text
+        }
+
+    }
+
 }
