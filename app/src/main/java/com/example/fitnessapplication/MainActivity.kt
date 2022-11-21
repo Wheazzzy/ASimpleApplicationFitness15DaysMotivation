2dcc6a7da015dd4e1df734f9924f3aff2c82b9c3
XXAUROR <200103001@stu.sdu.edu.kz>
2022-11-21 14:29:34 +0600

    Connecting Fragment to main activty

--- app/src/main/java/com/example/fitnessapplication/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.fitnessapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fitnessapplication.Fragments.MainFragmentDays
+import com.example.fitnessapplication.Utils.FragmentManager
+import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FragmentManager.setFragment(MainFragmentDays.newInstance(),this)
     }
 }
