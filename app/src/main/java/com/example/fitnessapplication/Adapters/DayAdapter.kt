1e93bc4d8f484fd37dade0a353ec4fe3d3e7f7d3
XXAUROR <200103001@stu.sdu.edu.kz>
2022-11-21 21:26:24 +0600

    Updated Class DayAdapter

--- app/src/main/java/com/example/fitnessapplication/Adapters/DayAdapter.kt
+++ app/src/main/java/com/example/fitnessapplication/Adapters/DayAdapter.kt
@@ -3,14 +3,18 @@ package com.example.fitnessapplication.Adapters
 import android.view.View
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fitnessapplication.R
 import com.example.fitnessapplication.databinding.DaysListItemBinding
 
-class DayAdapter: ListAdapter<DayModel, > {
+class DayAdapter: ListAdapter<DayModel, DayAdapter.DayHolder> {
 
     class DayHolder(view: View): RecyclerView.ViewHolder(view){
         private val binding = DaysListItemBinding.bind(view)
         fun settingData(day: DayModel) = with(binding){
-            names.text
+            val name = root.context.getString(R.string.day) + " ${adapterPosition + 1}"
+            names.text = name
+            val exercisesCounter = day.exercises.split(",").size.toString()
+            counters.text = exercisesCounter
         }
 
     }
