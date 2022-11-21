af5efa33bbf7211dd9eece03cd86f23e1be664ed
XXAUROR <200103001@stu.sdu.edu.kz>
2022-11-21 14:18:57 +0600

    Initializing transaction updated

--- app/src/main/java/com/example/fitnessapplication/Utils/FragmentManager.kt
+++ app/src/main/java/com/example/fitnessapplication/Utils/FragmentManager.kt
@@ -2,9 +2,12 @@ package com.example.fitnessapplication.Utils
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.fitnessapplication.R
 
 object FragmentManager {
     fun setFragment(newFragment: Fragment, act: AppCompatActivity){
         val transaction = act.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.place_holder, newFragment)
+        transaction.commit()
     }
 }
