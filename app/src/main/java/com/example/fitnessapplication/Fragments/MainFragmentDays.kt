1fcf923689bf6a82580094bb06e33006a3f97930
XXAUROR <200103001@stu.sdu.edu.kz>
2022-11-26 20:38:34 +0600

    Created Function initRcView and OnViewCreated for days_exersizes

--- app/src/main/java/com/example/fitnessapplication/Fragments/MainFragmentDays.kt
+++ app/src/main/java/com/example/fitnessapplication/Fragments/MainFragmentDays.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fitnessapplication.Adapters.DayAdapter
 import com.example.fitnessapplication.Adapters.DayModel
 import com.example.fitnessapplication.R
 import com.example.fitnessapplication.databinding.FragmentMainDaysBinding
 
 
-class MainFragmentDays : Fragment() {
+class MainFragmentDays: Fragment() {
     private lateinit var binding: FragmentMainDaysBinding
 
     override fun onCreateView(
@@ -18,6 +21,18 @@ class MainFragmentDays : Fragment() {
         savedInstanceState: Bundle?): View {
         binding = FragmentMainDaysBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializationRecyclerView()
+    }
+
+    private fun initializationRecyclerView() = with(binding){
+        var adapter = DayAdapter()
+        recyclerViewDays.layoutManager = LinearLayoutManager(activity as AppCompatActivity)
+        recyclerViewDays.adapter = adapter
+        adapter.submitList(fillDaysArray())
     }
 
     private fun fillDaysArray(): ArrayList<DayModel>{
--- app/src/main/java/com/example/fitnessapplication/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.fitnessapplication.Utils.FragmentManager
 import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
