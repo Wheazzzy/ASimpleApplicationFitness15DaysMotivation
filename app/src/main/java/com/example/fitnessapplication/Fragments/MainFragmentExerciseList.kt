2b17f3804e3dcd15e8e90a6753499acbcce177d7
XXAUROR <200103001@stu.sdu.edu.kz>
2022-11-27 16:21:47 +0600

    Updated

--- app/src/main/java/com/example/fitnessapplication/Fragments/MainFragmentExerciseList.kt
+++ app/src/main/java/com/example/fitnessapplication/Fragments/MainFragmentExerciseList.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.fitnessapplication.Utils.MainViewModel
 import com.example.fitnessapplication.databinding.ExercisesListFragmentBinding
 import com.example.fitnessapplication.databinding.FragmentMainDaysBinding
 
 
 class MainFragmentExerciseList: Fragment() {
     private lateinit var binding: ExercisesListFragmentBinding
+    private val model: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,10 +24,10 @@ class MainFragmentExerciseList: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        model.mutableExerciseList.observe(viewLifecycleOwner){
 
+        }
     }
-
-
 
     companion object {
         @JvmStatic
