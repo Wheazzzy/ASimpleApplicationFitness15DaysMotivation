9ea1bbfeaeaca296778246065aaf0e16b8ccf751
XXAUROR <200103001@stu.sdu.edu.kz>
2022-11-27 19:41:02 +0600

    Added binding to progress bar and connect their andr logic about timer

--- app/src/main/java/com/example/fitnessapplication/Fragments/MainWaitingFragment.kt
+++ app/src/main/java/com/example/fitnessapplication/Fragments/MainWaitingFragment.kt
@@ -11,38 +11,45 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fitnessapplication.Adapters.ExerciseAdapter
 import com.example.fitnessapplication.Utils.MainViewModel
 import com.example.fitnessapplication.databinding.ExercisesListFragmentBinding
+import com.example.fitnessapplication.databinding.WaitingFragmentBinding
 
 
 const val COUNT_DOWN_TIMER = 11000L
 
 class MainWaitingFragment: Fragment() {
-    private lateinit var binding: ExercisesListFragmentBinding
+    private lateinit var binding: WaitingFragmentBinding
     private lateinit var timer: CountDownTimer
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View {
-        binding = ExercisesListFragmentBinding.inflate(inflater, container, false)
+        binding = WaitingFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.progressBar.max = COUNT_DOWN_TIMER.toInt()
 
     }
 
     private fun startingTime() = with(binding){
         timer = object: CountDownTimer(COUNT_DOWN_TIMER, 100){
-            override fun onTick(p0: Long) {
-
+            override fun onTick(resultTime: Long) {
+                progressBar.progress = resultTime.toInt()
             }
 
             override fun onFinish() {
 
             }
 
-        }
+        }.start()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        timer.cancel()
     }
 
     companion object {
