4bfb5fc412d5bca42303db5baae8655d455c1c62
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-02 21:17:12 +0600

    Created fun SavedPref for days

--- app/src/main/java/com/example/fitnessapplication/Utils/MainViewModel.kt
+++ app/src/main/java/com/example/fitnessapplication/Utils/MainViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.fitnessapplication.Utils
 
+import android.content.SharedPreferences
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.fitnessapplication.Adapters.ExerciseModel
 
 class MainViewModel: ViewModel() {
     val mutableExerciseList = MutableLiveData<ArrayList<ExerciseModel>>()
+    val preferences: SharedPreferences? = null
+
+    fun savePreferences(key: String, exerciseCounter: Int){
+        preferences?.edit()?.putInt(key, exerciseCounter)?.apply()
+    }
 }
