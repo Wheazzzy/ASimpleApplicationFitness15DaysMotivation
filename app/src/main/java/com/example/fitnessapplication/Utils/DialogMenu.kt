6172051505cdd605c05f49800fa6d4679fe9eefa
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-03 20:42:54 +0600

    In fun showDiaolog 2 button

--- app/src/main/java/com/example/fitnessapplication/Utils/DialogMenu.kt
+++ app/src/main/java/com/example/fitnessapplication/Utils/DialogMenu.kt
@@ -1,9 +1,20 @@
 package com.example.fitnessapplication.Utils
 
+import android.app.AlertDialog
 import android.content.Context
+import com.example.fitnessapplication.R
 
 class DialogMenu {
-    fun showDialog(context: Context){
+    fun showDialog(context: Context, massageId: Int){
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(R.string.attention)
+        builder.setMessage(massageId)
+        builder.setPositiveButton(R.string.reset) { _,_ ->
 
+        }
+    }
+
+    interface Listener {
+        fun onClick()
     }
 }
