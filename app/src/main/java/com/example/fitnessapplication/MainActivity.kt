1078b418dd60aac10f0d059403e281eddd62c4ff
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-10 13:31:42 +0600

    added more execise

--- app/src/main/java/com/example/fitnessapplication/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapplication/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitnessapplication.Fragments.MainFragmentDays
-//import com.example.fitnessapplication.Utils.ConnectivityObserver
 import com.example.fitnessapplication.Utils.FragmentManager
 import com.example.fitnessapplication.Utils.MainViewModel
-//import com.example.fitnessapplication.Utils.NetworkConnectivityObserver
+
+
 
 class MainActivity : AppCompatActivity() {
     private val model: MainViewModel by viewModels()
-//    private lateinit var connectivityObserver: ConnectivityObserver
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        connectivityObserver = NetworkConnectivityObserver(applicationContext)
         setContentView(R.layout.activity_main)
         model.preferences = getSharedPreferences("main", MODE_PRIVATE)
         FragmentManager.setFragment(MainFragmentDays.newInstance(), this)
