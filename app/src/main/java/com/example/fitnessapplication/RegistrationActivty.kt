d26c42819f0ac9403c517a446acbd661ba69cd45
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-11 14:23:21 +0600

    Added firebase for login

--- app/src/main/java/com/example/fitnessapplication/RegistrationActivty.kt
+++ app/src/main/java/com/example/fitnessapplication/RegistrationActivty.kt
@@ -1,11 +1,21 @@
 package com.example.fitnessapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegistrationActivty : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_activty)
+
+        val loginText: TextView = findViewById(R.id.textView_registered)
+        loginText.setOnClickListener{
+            val intent = Intent(this,LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
