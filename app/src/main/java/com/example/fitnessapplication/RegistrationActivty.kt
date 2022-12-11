46b92d973484d91fbd161b8008aa4d8880225da4
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-11 14:36:25 +0600

    Creared fun signUP

--- app/src/main/java/com/example/fitnessapplication/RegistrationActivty.kt
+++ app/src/main/java/com/example/fitnessapplication/RegistrationActivty.kt
@@ -5,17 +5,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegistrationActivty : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_activty)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         val loginText: TextView = findViewById(R.id.textView_registered)
         loginText.setOnClickListener{
             val intent = Intent(this,LogInActivity::class.java)
             startActivity(intent)
         }
+        signUp()
     }
+
+    private fun signUp(){
+
+    }
+
 }
