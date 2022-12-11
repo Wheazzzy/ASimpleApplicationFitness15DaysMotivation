1fb6438b44befe99675d9eb807a32a23743d2a3e
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-11 14:59:03 +0600

    Creared rule for regi-n act-y

--- app/src/main/java/com/example/fitnessapplication/RegistrationActivty.kt
+++ app/src/main/java/com/example/fitnessapplication/RegistrationActivty.kt
@@ -2,13 +2,12 @@ package com.example.fitnessapplication
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.util.Log.e
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fitnessapplication.databinding.ExercisesListFragmentBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -25,16 +24,22 @@ class RegistrationActivty : AppCompatActivity() {
         auth = Firebase.auth
 
         val loginText: TextView = findViewById(R.id.textView_registered)
-        loginText.setOnClickListener{
-            val intent = Intent(this,LogInActivity::class.java)
+        loginText.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
             startActivity(intent)
         }
         signUp()
     }
 
-    private fun signUp(){
+    private fun signUp() {
         val email = findViewById<EditText>(R.id.editTextTextRegistration)
         val password = findViewById<EditText>(R.id.editTextTextPasswordRegistration)
+
+        if (email.text.isEmpty() || password.text.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields again", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
 
         val inputEmail = email.text.toString()
         val inputPassword = password.text.toString()
@@ -43,13 +48,26 @@ class RegistrationActivty : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this, ExercisesListFragmentBinding::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext, "Success :)",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
                 } else {
                     // If sign in fails, display a message to the user.
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
-
+            .addOnFailureListener {
+                Toast.makeText(this, "Error access ${it.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+            }
     }
 
 }
