98e46c09896f1e2f798c6fdd1dffd927561267e8
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-11 14:02:49 +0600

    Aded listener for registerNow

--- app/src/main/java/com/example/fitnessapplication/LogInActivity.kt
+++ app/src/main/java/com/example/fitnessapplication/LogInActivity.kt
@@ -1,11 +1,21 @@
 package com.example.fitnessapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LogInActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        val registerText: TextView = findViewById(R.id.textView_registerNow)
+        registerText.setOnClickListener{
+            val intent = Intent(this, RegistrationActivty::class.java)
+            startActivity(intent)
+        }
     }
 }
